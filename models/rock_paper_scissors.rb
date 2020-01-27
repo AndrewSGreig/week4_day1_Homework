546c7e518319088abdd934e027d01b7dd878eaaa
User <user@Users-MacBook-Pro.local>
2020-01-27 17:03:56 +0000

    improving output

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -9,34 +9,33 @@ class RockPaperScissors
     p "player1_shot = #{player1_shot}"
     p "player2_shot = #{player2_shot}"
     if player1_shot == player2_shot
-      return player1_shot
+      return "draw both played #{player1_shot}"
     end
-      return player2_shot
-    end
+    #   return player2_shot
+    # end
 
     if player1_shot == 'rock'
       if player2_shot == 'scissors'
-        return player1_shot
+        return "player 1 wins with #{player1_shot}"
       elsif player2_shot == 'paper'
-        return player2shot
+        return "player2 wins with #{player2_shot}"
       end
     end
 
     if player1_shot == 'scissors'
       if player2_shot == 'paper'
-        return player1_shot
+        return "player 1 wins with #{player1_shot}"
       elsif player2_shot == 'rock'
-        return player2_shot
+        return "player2 wins with #{player2_shot}"
       end
     end
 
     if player1_shot == 'paper'
       if player2_shot == 'rock'
-        return player1_shot
+        return "player 1 wins with #{player1_shot}"
       elsif player2_shot == 'scissors'
-        return player2_shot
+        return "player2 wins with #{player2_shot}"
       end
     end
   end
-
 end
